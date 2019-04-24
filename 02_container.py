6e9ee9c51fa12528aab046f095febd1a374fbc2c
Youngtaek Hong <hoo00050@naver.com>
2019-04-24 10:37:58 +0000

    first commmit

---
+++ 02_container.py
@@ -0,0 +1,20 @@
+a = dict()
+# a = {'이름': '김재석', '나이':'19'}
+a['js'] = 29
+a['ty'] = 29
+a['th'] = 29
+a['dj'] = 34
+
+print(a)
+
+# b = dict()
+# b['서울'] = 02
+# b['']
+# phone_book = {'서울' : '02', '경기' : '031', '제주' : '064'}
+# print(phone_book['제주'])
+
+my_account = {'bank' : '우리은행', 'balance' : 235, 'pay' : 100}
+print(my_account['bank'])
+print(my_account.get['lotto'])
+
+# 각 지역 번호가 담긴 딕셔너리를 만들어보세요 서울 - 02/ 
---
+++ 03_loop.py
@@ -0,0 +1,28 @@
+# # n = 0
+# # while n < 5:
+# #     print('대표님, 열심히 일하겠습니다.')
+# #     n = n + 1
+
+# foods = ['pizza', 'pasta', '떡볶이', '순대']
+
+# for food in foods:
+#     print(food)
+
+new_numbers = []
+.append()
+
+numbers = [1, 2, 3, 4, 5, 6]
+[2, 4, 6, 8, 10, 12]
+# numbers에서 for문을 사용해 요소를 꺼내, 각각의 요소에 2를 곱해주세요.
+
+new_numbers = []
+for number in numbers:
+
+    
+    new_numbers.append(number*2)
+    
+print(new_numbers)
+# 추가미션 : 2를 곱한 값을 new_numbers 라는 새로운 리스트에 담아주세요.
+
+# 추가미션2 : 구구단 만들기 2단부터 9단까지 쫙 출력하기. - 2중 for 문을 사용하면 된다.
+
